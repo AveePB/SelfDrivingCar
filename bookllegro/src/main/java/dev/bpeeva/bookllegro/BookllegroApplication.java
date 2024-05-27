6e6cf5776f416d1ada2d9c05ff00d3e93ccbc783
AveePB <mike303.py@gmail.com>
2024-05-27 23:06:44 +0200

    Clean project

---
+++ bookllegro/src/main/java/dev/bpeeva/bookllegro/BookllegroApplication.java
@@ -0,0 +1,13 @@
+package dev.bpeeva.bookllegro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookllegroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookllegroApplication.class, args);
+	}
+
+}
