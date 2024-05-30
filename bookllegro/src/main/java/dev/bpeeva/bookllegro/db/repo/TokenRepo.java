8ba6bbf273d47f24e9b0c6860a07992285aa4802
AveePB <mike303.py@gmail.com>
2024-05-30 15:22:37 +0200

    Added token repo method

--- bookllegro/src/main/java/dev/bpeeva/bookllegro/db/repo/TokenRepo.java
+++ bookllegro/src/main/java/dev/bpeeva/bookllegro/db/repo/TokenRepo.java
@@ -1,6 +1,7 @@
 package dev.bpeeva.bookllegro.db.repo;
 
 import dev.bpeeva.bookllegro.db.model.Token;
+import dev.bpeeva.bookllegro.db.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,5 @@ import java.util.Optional;
 public interface TokenRepo extends JpaRepository<Token, Integer> {
 
     Optional<Token> findByRawForm(String rawForm);
+    Optional<Token> findByUser(User user);
 }
