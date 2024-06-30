1ce714603fd3eddeb62cdca0d2f11560f80ce7c4
AveePB <mike303.py@gmail.com>
2024-06-30 23:49:19 +0200

    inital commit

---
+++ src/MotorControl.h
@@ -0,0 +1,17 @@
+#include <AFMotor.h>
+
+#ifndef MOTORCONTROL_H
+#define MOTORCONTROL_H
+
+namespace CarMovement {
+  // Variables
+  extern int motorSpeed, turnSpeed; 
+
+  // Functions
+  void moveForward(AF_DCMotor* rightBack, AF_DCMotor* rightFront, AF_DCMotor* leftFront, AF_DCMotor* leftBack);
+  void turnLeft(AF_DCMotor* rightBack, AF_DCMotor* rightFront, AF_DCMotor* leftFront, AF_DCMotor* leftBack);
+  void turnRight(AF_DCMotor* rightBack, AF_DCMotor* rightFront, AF_DCMotor* leftFront, AF_DCMotor* leftBack);
+  void stop(AF_DCMotor* rightBack, AF_DCMotor* rightFront, AF_DCMotor* leftFront, AF_DCMotor* leftBack);
+}
+
+#endif
---
+++ src/maker-uno-obstacle-avoiding-robot.ino
@@ -0,0 +1,45 @@
+#include "MotorControl.h"
+#include <AFMotor.h>
+#include <Servo.h>
+
+// DC Motor pins
+#define RB_PIN 1
+#define RF_PIN 2
+#define LF_PIN 3
+#define LB_PIN 4
+
+#define BROADCAST_PORT 9600
+#define SERVO_PIN 10
+
+// Controllable components
+AF_DCMotor rb_motor(RB_PIN), rf_motor(RF_PIN), lf_motor(LF_PIN), lb_motor(LB_PIN);
+Servo servoLook;
+
+void setup() {
+  // Establish communication
+  Serial.begin(BROADCAST_PORT);
+
+  // Set up servo
+  servoLook.attach(SERVO_PIN);
+
+  // Set up motor speeds
+  rb_motor.setSpeed(CarMovement::motorSpeed);
+  rf_motor.setSpeed(CarMovement::motorSpeed);
+  lf_motor.setSpeed(CarMovement::motorSpeed);
+  lb_motor.setSpeed(CarMovement::motorSpeed);
+
+  // Set up motor modes
+  rb_motor.run(RELEASE);
+  rf_motor.run(RELEASE);
+  lf_motor.run(RELEASE);
+  lb_motor.run(RELEASE);  
+}
+
+void loop() {
+  rb_motor.run(FORWARD);
+  rf_motor.run(FORWARD);
+  lf_motor.run(FORWARD);
+  lb_motor.run(FORWARD);  
+  
+
+}
